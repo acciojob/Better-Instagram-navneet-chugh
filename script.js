8340ad73365667d64bea46bd1bb7fbaa6d8953ad
navneet-chugh <navneetchugh15@gmail.com>
2023-08-20 16:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+document.addEventListener("DOMContentLoaded", function() {
+  const images = document.querySelectorAll(".image");
+  let selectedImage = null;
+
+  images.forEach(image => {
+    image.addEventListener("dragstart", function(e) {
+      selectedImage = this;
+    });
+
+    image.addEventListener("dragover", function(e) {
+      e.preventDefault();
+    });
+
+    image.addEventListener("drop", function(e) {
+      e.preventDefault();
+      if (selectedImage !== null && selectedImage !== this) {
+        // Swap background images
+        const tempImage = this.style.backgroundImage;
+        this.style.backgroundImage = selectedImage.style.backgroundImage;
+        selectedImage.style.backgroundImage = tempImage;
+      }
+    });
+  });
+});
